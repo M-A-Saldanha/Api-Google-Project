7505ad73786fd3df9218638a145770e094230f70
M-A-Saldanha <marco.antonio.saldanha@hotmail.com>
2021-11-10 19:03:56 -0300

    Projeto finalizado

---
+++ src/components/ImageCard/index.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Skeleton from "../Skeleton/";
+
+const Card = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+    width: 90px;
+    height: 90px;
+    border-radius: 6px;
+    background-image: url(${(props) => props.photo});
+    background-size: cover;
+`;
+
+const Title = styled.span`
+    font-family: ${(props) => props.theme.fonts.regular};
+    color: #fff;
+    font-size: 16px;
+`;
+
+const ImageCard = ({ photo, title }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const imageLoaded = new Image();
+        imageLoaded.src = photo;
+        imageLoaded.onload = () => setImageLoaded(true);
+    }, [photo])
+
+    return (
+        <>
+            {imageLoaded ? (
+                <Card photo={photo}>
+                    <Title>{title}</Title>
+                </Card>
+            ) : (
+                <Skeleton width="90px" height="90px" />
+            )}
+        </>
+    );
+};
+
+export default ImageCard;
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+        primary: '#2980b9',
+        background: '#ecf0f1',
+        text: '#2c3e50',
+        success: '#2ecc71',
+    },
+    fonts: {
+        regular: 'Roboto, sans-serif',
+    },
+};
+
+export default theme
